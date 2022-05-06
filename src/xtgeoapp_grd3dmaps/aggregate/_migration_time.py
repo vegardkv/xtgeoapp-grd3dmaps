70ccee5374c8ff8ea17f61f2bbdd21549f6163c7
Vegard Berg Kvernelv <vegard@nr.no>
2022-05-06 09:19:46 +0200

    Add TODO comment

--- src/xtgeoapp_grd3dmaps/aggregate/_migration_time.py
+++ src/xtgeoapp_grd3dmaps/aggregate/_migration_time.py
@@ -15,6 +15,7 @@ def generate_migration_time_property(
     ]
     time_since_start = [(t - times[0]).days / 365 for t in times]
     # Duplicate first property to ensure equal actnum
+    # TODO: expose property name. At a minimum as a non-anonymous variable.
     t_prop = co2_props[0].copy('MigrationTime')
     t_prop.values[~t_prop.values.mask] = np.inf
     for co2, t in zip(co2_props, time_since_start):
